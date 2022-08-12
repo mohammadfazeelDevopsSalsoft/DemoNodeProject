a6e4bfb531e2e4f9b124d8a359aba288b14ff63b
mohammadfazeelDevopsSalsoft <106160157+mohammadfazeelDevopsSalsoft@users.noreply.github.com>
2022-08-12 17:03:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-	  res.send("Hello to 1first pipeline");
+	  res.send("Hello to pipeline");
 });
 
 app.listen(4000);
